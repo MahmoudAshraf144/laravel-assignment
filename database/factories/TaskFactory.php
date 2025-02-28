5f277a9110d98aee772d3a1daaec4ec2c3f2c049
Mahmoud Ashraf Fouad <mah.ashraf144@gmail.com>
2025-03-01 00:30:24 +0200

    Initial commit

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'status' => $this->faker->randomElement(['pending', 'in_progress', 'completed']),
+            'user_id' => User::factory(),
+        ];
+    }
+}
